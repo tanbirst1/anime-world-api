92e2d00ec5df4bf0c081e2365a15da7910dd78b3
tanbirst1 <162620948+tanbirst1@users.noreply.github.com>
2025-07-30 21:49:12 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+export default async function handler(req, res) {
+  try {
+    const url = `https://watchanimeworld.in/`;
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+
+    let homeData = [];
+    $(".item a").each((i, el) => {
+      const title = $(el).attr("title");
+      const link = $(el).attr("href");
+      const img = $(el).find("img").attr("src");
+      if (title && link) {
+        homeData.push({
+          title,
+          link: link.startsWith("http") ? link : `https://watchanimeworld.in${link}`,
+          image: img
+        });
+      }
+    });
+
+    res.status(200).json(homeData);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch homepage" });
+  }
+}
