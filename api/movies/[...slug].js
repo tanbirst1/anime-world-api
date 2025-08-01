19cf0b6f6d1a8c720b88fb9d0eec922249e3beb6
tanbirst1 <162620948+tanbirst1@users.noreply.github.com>
2025-08-01 23:24:52 +0530

    Create [...slug].js

---
+++ api/movies/[...slug].js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+  const slugPath = req.query.slug; 
+  const slug = Array.isArray(slugPath) ? slugPath.join('/') : slugPath;
+
+  if (!slug) {
+    return res.status(400).json({ error: "Slug missing" });
+  }
+
+  res.status(200).json({
+    status: "ok",
+    slug,
+    message: "Dynamic route works perfectly"
+  });
+    }
