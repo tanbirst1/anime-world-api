c87e62fae9d205e029d67c48a56a79061b5e6b2b
tanbirst1 <162620948+tanbirst1@users.noreply.github.com>
2025-08-08 21:04:09 +0530

    Create season.js

---
+++ api/series/season.js
@@ -0,0 +1,6 @@
+import { getSeasonData } from "./slug.js";
+
+export default async function handler(req, res) {
+  const season = parseInt(req.query.season || "1");
+  return getSeasonData(req, res, season);
+}
